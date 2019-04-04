347204c805e787c9d9da025b0b6aafc495b62cc1
yatoogamii <bracaval.elias.pro@gmail.com>
2019-04-04 20:56:46 +0200

    add function for addLi, crossedItem, removeItem, onlickButton and onPresskeyEnter

--- js/main.js
+++ js/main.js
@@ -8,18 +8,69 @@
 
 let button = document.getElementsByClassName("button")[0];
 let ul = document.getElementsByClassName('TODO-list')[0];
+let input = document.getElementById('add-item');
+let spans;
+let inputValue = '';
+let icons;
 
-
+console.log(ul);
 /////////////////
 //  functions  //
 /////////////////
 
+function getInputValue() {
+  inputValue = input.value;
+}
+
+function checkInputEmptyValue() {
+  if (inputValue == '') {
+    return;
+  }
+}
+
+function addLiInList() {
+  if (inputValue != '') {
+    ul.insertAdjacentHTML('beforeEnd', `<li><i class="far fa-times-circle"></i><span>${inputValue[0].toUpperCase() + inputValue.slice(1)}</span></li>`);
+    input.value = '';
+  }
+}
+
+function crossedLi() {
+  spans = document.getElementsByTagName("span");
+  for (let span of spans) {
+    span.onclick = function() {
+      span.classList.toggle('crossed')
+      
+    }
+  }
+}
+
+function removeItem() {
+  icons = document.getElementsByClassName('far fa-times-circle');
+  for (let icon of icons) {
+    icon.onclick = function() {
+      icon.parentElement.outerHTML="";
+    }
+  }
+}
+
 button.onclick = function() {
-
-  let newLi = document.createElement('li');
-  let inputValue = document.getElementById('add-item').value;
-  newLi.innerHTML = inputValue;
-  ul.insertAdjacentHTML('beforeEnd', `<li> ${inputValue} </li>`);
-
+  getInputValue();
+  checkInputEmptyValue();
+  addLiInList();
+  crossedLi();
+  removeItem();
 };
+
+input.onkeypress = function(event) {
+  getInputValue();
+  if (event.which == 13 || event.keyCode == 13) {
+    checkInputEmptyValue();
+    addLiInList();
+    crossedLi();
+    removeItem();
+  }
+  return;
+}
+
 
